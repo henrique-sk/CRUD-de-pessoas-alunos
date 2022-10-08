c2176e1a2c8c0213eee93daa5ff271d15d8c3554
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-10-08 14:32:10 -0300

    criadas mais opções de menu

--- src/util/Menu.java
+++ src/util/Menu.java
@@ -6,7 +6,7 @@ public class Menu {
 			+ "--------------------------------------------\n"
 			+ "Digite a opção desejada:\n";
 	
-	public static final void menu1() {
+	public static final void menuPrincipal() {
 		System.out.println(""
 				+ "============================================\n"
 				+ "Bem-vindo ao sistema de gerenciamento de pessoas/alunos\n"
@@ -16,6 +16,19 @@ public class Menu {
 				+ "3 - Atualizar dados de pessoas/alunos\n"
 				+ "4 - Deletar uma pessoa/aluno"
 				+ "0 - Encerrar programa");
+	}
+	
+	public static final void menuMostrar() {
+		System.out.println(""
+				+ cabecalho
+				+ "1 - Mostrar todas pessoas\n"
+				+ "2 - Mostrar somente alunos");
+	}
+	
+	public static final void porID() {
+		System.out.println(""
+				+ "--------------------------------------------\n"
+				+ "Digite o número correspondente a pessoa/aluno desejada:\n");
 	}
 
 }
