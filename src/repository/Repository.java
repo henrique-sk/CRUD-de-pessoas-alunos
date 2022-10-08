4177b989e699e1abae10270ede5660f2eee66179
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-10-07 23:37:41 -0300

    criada classe repository

--- src/repository/Repository.java
+++ src/repository/Repository.java
@@ -1,5 +1,34 @@
 package repository;
 
-public class Repository {
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+import model.Banco;
+
+public class Repository <T extends Banco> {
+	
+	Map<Integer, T> bancoDeDados;
+	
+	public Repository() {
+		this.bancoDeDados = new TreeMap<>();
+	}
+	
+	public void salvar(T t) {
+		this.bancoDeDados.put(t.getId(), t);
+	}
+	
+	public List<T> buscarTodos() {
+		return this.bancoDeDados.values().stream().collect(Collectors.toList());
+	}
+	
+	public T buscarPorId(int id) {
+		return this.bancoDeDados.get(id);
+	}
+	
+	public void removerPorId(int id) {
+		this.bancoDeDados.remove(id);
+	}
 
 }
