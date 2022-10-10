befaf0e3bdf12089eab41a11efc588c1862cfe6f
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-10-10 00:21:43 -0300

    tratamento de erros

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import exception.SistemaException;
@@ -6,51 +7,64 @@ import util.Menu;
 
 public class Main {
 
-	public static void main(String[] args) throws SistemaException {
+	public static void main(String[] args) throws InterruptedException {
 		Scanner sc = new Scanner(System.in);
 		Service service = new Service(sc);
 
 		boolean continua = true;
 		do {
-			Menu.menuPrincipal();
-			int opcao1 = sc.nextInt();
-			sc.nextLine();
+			try {
+				Menu.menuPrincipal();
+				int opcao1 = sc.nextInt();
+				sc.nextLine();
 
-			switch (opcao1) {
-			case 1:
-				service.criarPessoaAluno();
-				break;
-			case 2:
-				Menu.menuMostrar();
-				int opcao2 = sc.nextInt();
-				if (opcao2 == 1) {
-					service.mostrarTodasPessoas();
-				} else if (opcao2 == 2) {
-					service.mostrarTodosAlunos();
-				}
-				break;
-			case 3:				
-				service.mostrarTodasPessoas();
-				System.out.println("Informe o número correspondente à pessoa que deseja atuaizar (ou 0 para retornar ao menu principal):");
-				int opcao3 = sc.nextInt();
-				String tipoPessoa;
-				if (opcao3 == 0) {
+				switch (opcao1) {
+				case 1:
+					service.criarPessoaAluno();
 					break;
-				} else {
-					tipoPessoa = service.tipoPessoa(opcao3);
+				case 2:
+					Menu.menuMostrar();
+					int opcao2 = sc.nextInt();
+					if (opcao2 == 1) {
+						service.mostrarTodasPessoas();
+					} else if (opcao2 == 2) {
+						service.mostrarTodosAlunos();
+					}
+					break;
+				case 3:
+					service.mostrarTodasPessoas();
+					System.out.println(
+							"Informe o número correspondente à pessoa que deseja atuaizar (ou 0 para retornar ao menu principal):");
+					int opcao3 = sc.nextInt();
+					String tipoPessoa;
+					if (opcao3 == 0) {
+						break;
+					} else {
+						tipoPessoa = service.tipoPessoa(opcao3);
+					}
+					service.atualizarDados(opcao3, tipoPessoa);
+					break;
+				case 4:
+					System.out.println("Informe o número correspondente à pessoa que deseja deletar:");
+					service.mostrarTodasPessoas();
+					int opcao4 = sc.nextInt();
+					service.deletarPessoa(opcao4);
+					break;
+				case 0:
+					System.out.println("Obrigado por utilizar nosso sistema. Até logo!");
+					continua = false;
+					break;
+				default:
+					System.out.println("Alternativa inválida. Tente novamente!");
+					break;
 				}
-				service.atualizarDados(opcao3, tipoPessoa);
-				break;
-			case 4:
-				System.out.println("Informe o número correspondente à pessoa que deseja deletar:");
-				service.mostrarTodasPessoas();				
-				int opcao4 = sc.nextInt();
-				service.deletarPessoa(opcao4);
-				break;
-			case 0:
-				System.out.println("Obrigado por utilizar nosso sistema. Até logo!");
-				continua = false;
-				break;
+			} catch (SistemaException e) {
+				System.out.println(e.getMessage());
+			} catch (InputMismatchException e) {
+				System.out.println("Opção inválida!!");
+				sc.next();
+			} finally {
+				Thread.sleep(1500l);
 			}
 
 		} while (continua);
